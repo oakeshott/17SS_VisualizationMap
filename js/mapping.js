f95f24edf546796c2b30a8fda1ba4bdd4f1cbd83
Takanori HARA <takanori.hara.pacino@gmail.com>
2017-08-02 23:36:28 +0900

    fixed

--- js/mapping.js
+++ js/mapping.js
@@ -21,8 +21,37 @@
   var div = d3.select("#map").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
+  // Overlay
+  var mapOverlay = L.d3SvgOverlay(function(sel, proj) {
+    var featureElement = sel.selectAll('path').data(features);
+    featureElement.enter()
+      .append("path")
+      .attr({
+        "d": proj.pathFromGeojson,
+        "stroke": 'gray',
+        "stroke-width": 2,
+      });
+    featureElement.on("mouseover", function(d) {
+      var str = "";
+      console.log(d.geometry.coordinates);
+      Object.keys(d.geometry.coordinates).forEach(function(key) { str += "[" + d.geometry.coordinates[key] + "]</br>" });
+      div.transition()
+        .duration(200)
+        .style("opacity", .9);
+      div.html(str)
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d) {
+      div.transition()
+        .duration(500)
+        .style("opacity", 0);
+    });
+  });
   var overlays = {
+    "Show a base map": mapOverlay,
     "Show a refuge": popups,
   };
+  d3.json("data/map.geojson",  function(data) { features = data.features; mapOverlay.addTo(map) });
   L.control.layers(baseLayers, overlays).addTo(map);
 })();
